8b37187e97b0dec0f61baa62f52bda38dbd7029b
Wiame5 <wiame.elouahabi5@gmail.com>
2026-02-25 15:53:13 +0000

    Test CI with a small change

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ const tasksRouter = require('./routes/tasks');
 app.get('/', (req, res) => {
   res.json({ message: "Welcome from FEATURE branch" }); 
 });
+// CI test change 
+console.log("Testing CI with a Pull Request"); 
 
 app.use('/tasks', tasksRouter); 
 
