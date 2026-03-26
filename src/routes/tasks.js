79e00e9629a8b1fc7dc1ee9d659c85d48fbf685d
Wiame5 <wiame.elouahabi5@gmail.com>
2026-03-26 17:37:08 +0100

    fix tasks

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -7,4 +7,14 @@ const tasks = [
 router.get('/', (req, res) => {
  res.json(tasks);
 }); 
+router.post('/', (req, res) => {
+  const newTask = {
+    id: tasks.length + 1,
+    title: req.body.title,
+    completed: req.body.completed || false
+  };
+
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+});
 module.exports = router; 
